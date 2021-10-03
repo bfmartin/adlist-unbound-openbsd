9cd11117a9ab43d8776e8d4479bc65b46817c0d8
bfmartin <https://www.bfmartin.ca/contact/>
2021-10-03 15:34:57 -0400

    corrected syntax error in previous commit

--- bin/adlist-unbound-fetch.sh
+++ bin/adlist-unbound-fetch.sh
@@ -9,7 +9,7 @@
 urls=" \
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts \
   https://adaway.org/hosts.txt \
-  https://pgl.yoyo.org/adservers/serverlist.php
+  https://pgl.yoyo.org/adservers/serverlist.php"
 # http://winhelp2002.mvps.org/hosts.txt"
 
 trap 'clean_up' EXIT
